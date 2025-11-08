20a27ea50c8b062ec035f4197c654d6062d91100
vuvufelix <vuvufelix7@gmail.com>
2025-11-08 15:51:45 -0200

    feat: add footer e cor na aplicação

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,7 +1,13 @@
+import "./Footer.css"
+import { FiLinkedin } from "react-icons/fi"
+
 const Footer = () => {
     return (
         <footer>
-            Footer
+            <div>
+                <small><a href="">LinkedIn do criador <FiLinkedin/></a></small>
+                <small>&copy; 2025 Nenet - Todos Direitos Reservados</small>
+            </div>
         </footer>
     )
 }
